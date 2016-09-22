7dae9a2c883604bf0f6538397ec1de825e551dae
TheAuror <czinegez@gmail.com>
2016-09-22 17:26:55 +0200

    Fileread update

--- src/main/java/com/mycompany/sampleproject/PresentationLayer/BinaryTreeServlet.java
+++ src/main/java/com/mycompany/sampleproject/PresentationLayer/BinaryTreeServlet.java
@@ -1,25 +1,14 @@
 package com.mycompany.sampleproject.PresentationLayer;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileUploadException;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 
 public class BinaryTreeServlet extends HttpServlet {
@@ -34,11 +23,18 @@ public class BinaryTreeServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException 
     {
-        //PrintWriter out = response.getWriter();
-        //out.print(request.getParameter("file"));
+        
          // Retrieves <input type="text" name="description">
-    Part filePart = request.getPart("file"); // Retrieves <input type="file" name="file">
-    String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); // MSIE fix.
-    InputStream fileContent = filePart.getInputStream();
+        Part filePart = request.getPart("file"); // Retrieves <input type="file" name="file">
+        String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); // MSIE fix.
+        InputStream fileContent = filePart.getInputStream();
+        PrintWriter out = response.getWriter();
+        String asd = "";
+        while(fileContent.available()>0)
+        {
+            asd+=(char)fileContent.read();
+            asd+=" ";
+        }
+        out.print(asd);
     }
 }
