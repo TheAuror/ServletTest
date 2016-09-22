69eb5b7a46aa3bf8c0362d0ba6c44ca161dce870
MarciThink <MarciThink@172.22.8.72>
2016-09-22 16:20:29 +0200

    Commit Message!

--- src/main/java/com/mycompany/sampleproject/PresentationLayer/BinaryTreeServlet.java
+++ src/main/java/com/mycompany/sampleproject/PresentationLayer/BinaryTreeServlet.java
@@ -1,11 +1,26 @@
 package com.mycompany.sampleproject.PresentationLayer;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
 
 public class BinaryTreeServlet extends HttpServlet {
     @Override
@@ -19,6 +34,11 @@ public class BinaryTreeServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException 
     {
-        System.out.println(request.getParameter("file"));
+        //PrintWriter out = response.getWriter();
+        //out.print(request.getParameter("file"));
+         // Retrieves <input type="text" name="description">
+    Part filePart = request.getPart("file"); // Retrieves <input type="file" name="file">
+    String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); // MSIE fix.
+    InputStream fileContent = filePart.getInputStream();
     }
 }
